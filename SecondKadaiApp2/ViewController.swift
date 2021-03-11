345711dab69107c3bef3a810f8657cf34c8f0edc
Ayase.Kadokawa <1119217x@g.hit-u.ac.jp>
2021-03-12 00:13:23 +0900

    Initial Commit

---
+++ SecondKadaiApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp2
+//
+//  Created by 広瀬綾香 on 2021/03/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
